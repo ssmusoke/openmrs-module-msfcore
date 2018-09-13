1d1fc9e2fa1a31b35183f7fc02e4fe4338b32971
Stephen S. Musoke <ssmusoke@gmail.com>
2018-09-13 15:25:51 +0300

    Added msfcore javascript file and mapping in webModuleApplicationContext.xml

--- omod/src/main/webapp/resources/scripts/msfcore.js
+++ omod/src/main/webapp/resources/scripts/msfcore.js
@@ -3,9 +3,12 @@ jQuery(document).ready(function() {
     // add a header section just above the form
     // Hide the register patient link
     jq('#register-patient-link').hide();
-    // change the text on the link
+    // remove the page heading
+    jq('#patient-search-form').prevAll('h2').remove();
+
+    // change the text on the register patient link
     jq('#patient-search-register-patient').html('+ ADD NEW PATIENT');
-    var content = '<div id="page-header">Search for Patient Record <a id="back" href="window.history.go(-1); return false;">Back</a> ' +jq('#patient-search-register-patient').outerHTML + '</div>'
+    var content = '<div id="page-header"><h2>Search for Patient Record</h2> <a class="button" id="find-patient-back" href="window.history.go(-1); return false;">< Back</a> ' +jq('#patient-search-register-patient')[0].outerHTML + '</div>'
 
     jq('#patient-search-form').before(content);
 });
